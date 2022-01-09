3dc052b866e6f540c2cf380b4bd91a01fe542bed
DiscreteTom <discrete_tom@outlook.com>
2022-01-09 18:38:51 +0800

    update page name

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,13 @@
           <v-list-item-action>
             <v-icon> mdi-file-replace-outline </v-icon>
           </v-list-item-action>
-          <v-list-item-content>Data Format Transform</v-list-item-content>
+          <v-list-item-content>Data Transformer</v-list-item-content>
         </v-list-item>
         <v-list-item exact to="/time">
           <v-list-item-action>
             <v-icon> mdi-clock-outline </v-icon>
           </v-list-item-action>
-          <v-list-item-content>Time Format Transform</v-list-item-content>
+          <v-list-item-content>Time Calculator</v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
