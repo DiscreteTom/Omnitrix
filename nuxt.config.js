cdfcb026d75394e4ef9cbdafaccd44ffb4c15135
DiscreteTom <discrete_tom@outlook.com>
2022-01-12 19:03:40 +0800

    update icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/png", href: "/icon.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
