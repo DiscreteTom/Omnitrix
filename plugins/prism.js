8308040f3114e1fe31e6a90bf780d0fad9b1ec65
DiscreteTom <discrete_tom@outlook.com>
2022-01-12 22:38:11 +0800

    add more languages for prismjs

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,8 +1,13 @@
 import Prism from "prismjs";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-python";
 import "prismjs/components/prism-yaml";
 import "prismjs/components/prism-json";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-matlab";
+import "prismjs/components/prism-css";
 import "prismjs/themes/prism-tomorrow.css";
 
 export default (context, inject) => {
